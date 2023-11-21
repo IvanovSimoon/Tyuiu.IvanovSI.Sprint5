f2e6955c80bc14d22a9f60aaeb48a34903d3bc73
Семён Иванов <lilepopendr1@mail.ru>
2023-11-21 20:23:30 +0500

    Task4

---
+++ Tyuiu.IvanovSI.Sprint5.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.IvanovSI.Sprint5.Task4.V11.Lib;
+using System.IO;
+namespace Tyuiu.IvanovSI.Sprint5.Task4.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V11.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double x = 2.61;
+            double y = Math.Round((Math.Sin(x) + (Math.Pow(x,2) / 2)), 3);
+            double res = 3.913;
+            Assert.AreEqual(y, res);
+        }
+    }
+}
---
+++ Tyuiu.IvanovSI.Sprint5.Task4.V11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanovSI.Sprint5.Task4.V11.Lib;
+using System.IO;
+namespace Tyuiu.IvanovSI.Sprint5.Task4.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Иванов С. И. | РПСб-23-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("* Задания #4                                                               *");
+            Console.WriteLine("* Вариант #11                                                              *");
+            Console.WriteLine("* Выполнил: Иванов Семён Иванович | РПСб-23-1                              *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Дан файл С:/DataSprint5/InPutDataFileTask4V0.txt в котором есть          *");
+            Console.WriteLine("* вещественное значение. Прочитать значение из файла и подставить вместо Х *");
+            Console.WriteLine("* в формуле (y=sinx + x^2/2). Вычислить значение по формуле                *");
+            Console.WriteLine("* и вернуть полученный результат на консоль.                               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask4V11.txt";
+            Console.WriteLine("Данные в файле: " + path);
+
+            Console.WriteLine();
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
